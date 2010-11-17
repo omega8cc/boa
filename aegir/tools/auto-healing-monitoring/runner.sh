8c71d5702ef62a9f185d3735e3b082c4a92a8d6e
Barracuda <omega8cc@gmail.com>
2010-11-17 13:49:20 +0100

    Use gitorious as master and github as mirror.

--- aegir/tools/auto-healing-monitoring/runner.sh
+++ aegir/tools/auto-healing-monitoring/runner.sh
@@ -2,30 +2,31 @@
 
 ###-------------SYSTEM-----------------###
 
-SERVERS="run-a run-b"
-
-###-------------SYSTEM-----------------###
 action()
 {
-NOW_LOAD=`awk '{print $1*100}' /proc/loadavg`
-CTL_LOAD=200
-if [ $NOW_LOAD -lt $CTL_LOAD ]; then
-echo load is $NOW_LOAD while maxload is $CTL_LOAD
-echo ... now doing CTL...
-for i in $SERVERS; do
-sh /var/xdrago/$i
-sleep 5
+for Runner in `find /var/xdrago -maxdepth 1 -type f | grep run- | uniq | sort`
+do
+  NOW_LOAD=`awk '{print $1*100}' /proc/loadavg`
+  CTL_LOAD=200
+  if [ $NOW_LOAD -lt $CTL_LOAD ]; then
+    echo load is $NOW_LOAD while maxload is $CTL_LOAD
+    echo running $Runner
+    bash $Runner
+    echo waiting 8 sec
+    sleep 8
+    echo CTL done
+  else
+    echo load is $NOW_LOAD while maxload is $CTL_LOAD
+    echo ...we have to wait...
+  fi
 done
-echo CTL done
-else
-echo load is $NOW_LOAD while maxload is $CTL_LOAD
-echo ...we have to wait...
-fi
 }
 
+###-------------SYSTEM-----------------###
+
 if test -f /var/xdrago/log/optimize_mysql_ao.pid ; then
-touch /var/xdrago/log/wait-runner
-exit
+  touch /var/xdrago/log/wait-runner
+  exit
 else
-action
+  action
 fi
