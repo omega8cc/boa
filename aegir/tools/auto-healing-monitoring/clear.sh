5fdbd8ec66e8259d3f178e3ac9e31ab57b2d4929
Barracuda <omega8cc@gmail.com>
2011-01-12 16:51:55 +0100

    Fix for php error logs rotate task (Grace)

--- aegir/tools/auto-healing-monitoring/clear.sh
+++ aegir/tools/auto-healing-monitoring/clear.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-rm -f /var/log/php/*
 /etc/init.d/php-fpm reload
+sleep 5
+echo rotate > /var/log/php/php-fpm-slow.log
+echo rotate > /var/log/php/php-fpm-error.log
+echo rotate > /var/log/php/error_log
 touch /var/xdrago/log/clear.done
