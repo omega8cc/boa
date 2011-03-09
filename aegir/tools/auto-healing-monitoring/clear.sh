c1aa88385e4eba77d835e3d1d8b11f3747845fac
Barracuda Team <admin@omega8.cc>
2011-03-09 20:10:29 +0100

    BOA: Fix for restricted access to private files in the Nginx configuration (Albert)

--- aegir/tools/auto-healing-monitoring/clear.sh
+++ aegir/tools/auto-healing-monitoring/clear.sh
@@ -5,4 +5,5 @@ sleep 5
 echo rotate > /var/log/php/php-fpm-slow.log
 echo rotate > /var/log/php/php-fpm-error.log
 echo rotate > /var/log/php/error_log
+echo rotate > /var/log/redis/redis-server.log
 touch /var/xdrago/log/clear.done
