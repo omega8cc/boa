a7cbc916bf49844bf974c21f0d3ff2d7878a9ad5
Barracuda Team <admin@omega8.cc>
2011-04-30 11:05:55 +0200

    BOA: Purge speed booster cache daily (Robert)

--- aegir/tools/auto-healing-monitoring/graceful.sh
+++ aegir/tools/auto-healing-monitoring/graceful.sh
@@ -11,13 +11,20 @@ action()
   rm -f -r /tmp/*
   rm -f /opt/tmp/*error*
   rm -f /opt/tomcat6/logs/*
-  touch /var/xdrago/log/graceful.done
+  rm -f -r /var/lib/nginx/speed/*
+  /etc/init.d/nginx stop
+  killall -9 nginx
+  /etc/init.d/nginx start
   rm -f /var/xdrago/log/wait-for-octopus-barracuda-running
+  touch /var/xdrago/log/graceful.done
 }
 
 if test -f /var/run/octopus_barracuda.pid ; then
   touch /var/xdrago/log/wait-for-octopus-barracuda-running
   exit
 else
+  touch /var/xdrago/log/optimize_mysql_ao.pid
+  sleep 60
   action
+  rm -f /var/xdrago/log/optimize_mysql_ao.pid
 fi
