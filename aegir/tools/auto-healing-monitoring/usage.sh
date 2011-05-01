fd779365b721697260579bf05153ca0d9014bf15
Barracuda Team <admin@omega8.cc>
2011-05-01 16:25:53 +0200

    BOA: Remove all existing robots.txt symlinks/files in all used platforms for robotstxt module compatibility (Robert)

--- aegir/tools/auto-healing-monitoring/usage.sh
+++ aegir/tools/auto-healing-monitoring/usage.sh
@@ -58,6 +58,9 @@ if [ ! -d "$Plr/cache/normal" ] ; then
   chown -R $_THIS_HM_USER:www-data $Plr/cache
   chmod -R 775 $Plr/cache
 fi
+if [ -e "$Plr/robots.txt" ] ; then
+  rm -f $Plr/robots.txt
+fi
 }
 
 fix_o_contrib_symlink()
