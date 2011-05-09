7743239a6d10d73814bbe96716a3d0ca13b8fdfd
Barracuda Team <admin@omega8.cc>
2011-05-09 16:12:29 +0200

    BOA: Use correct tune_memory_limits also in the BOND tuner (Robert)

--- aegir/tools/auto-healing-monitoring/minute.sh
+++ aegir/tools/auto-healing-monitoring/minute.sh
@@ -6,4 +6,22 @@ perl /var/xdrago/firewall/check/hackftp
 perl /var/xdrago/firewall/check/scan_nginx
 perl /var/xdrago/firewall/check/sqlcheck
 
+action()
+{
+limit=300
+xkill=null
+for each in `mysqladmin proc | awk '{print $2, $4, $8, $12}' | awk '{print $1}'`;
+do
+  xtime=`mysqladmin proc | awk '{print $2, $4, $8, $12}' | grep $each | awk '{print $4}'`
+  xuser=`mysqladmin proc | awk '{print $2, $4, $8, $12}' | grep $each | awk '{print $2}'`
+  if [ "$xtime" != "Time" ] && [ "$xuser" != "root" ] && [[ "$xtime" -gt "$limit" ]] ; then
+    xkill=`mysqladmin kill $each`
+    times=`date`
+    echo $times $each $xuser $xtime $xkill
+    echo "$times $each $xuser $xtime $xkill" >> /var/xdrago/log/sql_watch.log
+  fi;
+done
+}
+
+action
 echo DONE!
