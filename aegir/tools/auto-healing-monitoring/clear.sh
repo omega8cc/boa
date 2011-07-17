cee747b321e63ed7f1e1aa1f3676abbe7c290717
Barracuda Team <admin@omega8.cc>
2011-07-17 15:29:16 +0200

    BOA: Purge and restart Redis and Memcached caches hourly to avoid memory issues on some systems (Robert)

--- aegir/tools/auto-healing-monitoring/clear.sh
+++ aegir/tools/auto-healing-monitoring/clear.sh
@@ -11,4 +11,17 @@ if test -f /var/run/octopus_barracuda.pid ; then
 else
   rm -f -r /tmp/*
 fi
+killall memcached &> /dev/null
+bash /var/xdrago/memcache.sh
+invoke-rc.d redis-server stop 2>&1
+sleep 2
+rm -f /var/lib/redis/*
+rm -f /var/log/redis/*
+killall redis-server &> /dev/null
+rm -f /var/lib/redis/*
+sleep 2
+invoke-rc.d redis-server restart 2>&1
+sleep 2
+rm -f /var/lib/redis/*
+invoke-rc.d redis-server restart 2>&1
 touch /var/xdrago/log/clear.done
