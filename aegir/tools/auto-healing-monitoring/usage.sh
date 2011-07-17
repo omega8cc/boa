4ab05ae138fd32b38478175097131872f60376ab
Barracuda Team <admin@omega8.cc>
2011-07-17 11:17:40 +0200

    BOA: Deny supercron module if used - it may cause self-DoS (Robert)

--- aegir/tools/auto-healing-monitoring/usage.sh
+++ aegir/tools/auto-healing-monitoring/usage.sh
@@ -23,7 +23,7 @@
 _PERMISSIONS=YES
 _MODULES=YES
 _MODULES_ON="syslog cache path_alias_cache robotstxt filefield_nginx_progress"
-_MODULES_OFF="dblog update devel cookie_cache_bypass poormanscron"
+_MODULES_OFF="dblog update devel cookie_cache_bypass poormanscron supercron"
 
 
 ###-------------SYSTEM-----------------###
