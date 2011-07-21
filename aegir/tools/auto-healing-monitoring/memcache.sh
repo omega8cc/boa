be8ff5cfcf4d7355dc137561af7e4130347a3089
Barracuda Team <admin@omega8.cc>
2011-07-21 22:04:33 +0200

    Revert "BOA: Tune Memcached bins (Robert)"
    This reverts commit 49cafed96e7bbcf69ec4d3a6b672e599b47c7760.

--- aegir/tools/auto-healing-monitoring/memcache.sh
+++ aegir/tools/auto-healing-monitoring/memcache.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/usr/bin/memcached -u nobody -p 11211 -m 64 -I 3m -d -l 127.0.0.1
-/usr/bin/memcached -u nobody -p 11212 -m 64 -I 3m -d -l 127.0.0.1
-/usr/bin/memcached -u nobody -p 11213 -m 64 -I 3m -d -l 127.0.0.1
-/usr/bin/memcached -u nobody -p 11214 -m 64 -I 3m -d -l 127.0.0.1
-/usr/bin/memcached -u nobody -p 11215 -m 64 -I 3m -d -l 127.0.0.1
-/usr/bin/memcached -u nobody -p 11216 -m 64 -I 3m -d -l 127.0.0.1
-/usr/bin/memcached -u nobody -p 11217 -m 64 -I 3m -d -l 127.0.0.1
-/usr/bin/memcached -u nobody -p 11218 -m 64 -I 3m -d -l 127.0.0.1
+/usr/bin/memcached -u nobody -p 11211 -m 64 -d -l 127.0.0.1
+/usr/bin/memcached -u nobody -p 11212 -m 64 -d -l 127.0.0.1
+/usr/bin/memcached -u nobody -p 11213 -m 64 -d -l 127.0.0.1
+/usr/bin/memcached -u nobody -p 11214 -m 64 -d -l 127.0.0.1
+/usr/bin/memcached -u nobody -p 11215 -m 64 -d -l 127.0.0.1
+/usr/bin/memcached -u nobody -p 11216 -m 64 -d -l 127.0.0.1
+/usr/bin/memcached -u nobody -p 11217 -m 64 -d -l 127.0.0.1
+/usr/bin/memcached -u nobody -p 11218 -m 64 -d -l 127.0.0.1
