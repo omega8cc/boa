8bb9a0000b9b5ea01186013dbb23961b19e26b08
Barracuda Team <admin@omega8.cc>
2011-12-08 22:06:52 +0100

    BOA: preg_match for dev should check for dev. and devel. only - the dot is required to avoid redirecting spiders to www.aegirproject.org for domains like world-class-development - and to avoid disabling caching and CSS/JS aggregation (Robert)

--- aegir/tools/auto-healing-monitoring/minute.sh
+++ aegir/tools/auto-healing-monitoring/minute.sh
@@ -6,6 +6,16 @@ perl /var/xdrago/firewall/check/hackftp
 perl /var/xdrago/firewall/check/scan_nginx
 perl /var/xdrago/firewall/check/sqlcheck
 
+killit()
+{
+  if [ "$xtime" != "Time" ] && [ "$xuser" != "root" ] && [[ "$xtime" -gt "$limit" ]] ; then
+    xkill=`mysqladmin kill $each`
+    times=`date`
+    echo $times $each $xuser $xtime $xkill
+    echo "$times $each $xuser $xtime $xkill" >> /var/xdrago/log/sql_watch.log
+  fi
+}
+
 action()
 {
 limit=600
@@ -16,12 +26,9 @@ do
   xuser=`mysqladmin proc | awk '{print $2, $4, $8, $12}' | grep $each | awk '{print $2}'`
   if [ "$xtime" != "Time" ] && [ "$xuser" = "xabuse" ] ; then
     limit=60
-  fi
-  if [ "$xtime" != "Time" ] && [ "$xuser" != "root" ] && [[ "$xtime" -gt "$limit" ]] ; then
-    xkill=`mysqladmin kill $each`
-    times=`date`
-    echo $times $each $xuser $xtime $xkill
-    echo "$times $each $xuser $xtime $xkill" >> /var/xdrago/log/sql_watch.log
+    killit
+  else
+    limit=600
   fi;
 done
 }
