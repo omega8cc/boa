6757bb64ec7d3d64bad23c2c33d8edfd15efe6c5
Barracuda Team <admin@omega8.cc>
2011-12-28 12:23:29 +0100

    BOA: Simplify mysql_backup.sh code (Robert)

--- aegir/tools/auto-healing-monitoring/mysql_backup.sh
+++ aegir/tools/auto-healing-monitoring/mysql_backup.sh
@@ -9,66 +9,36 @@ DATE=`date +%y%m%d-%H%M`
 HOST=`hostname`
 SAVELOCATION=$BACKUPDIR/$HOST-$DATE
 
+truncate_cache_tables() {
+TABLES="sessions cache cache_menu cache_page cache_filter cache_form cache_block cache_views_data cache_views cache_content watchdog cache_path boost_cache boost_cache_relationships"
+for i in $TABLES; do
+mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
+TRUNCATE $i;
+EOFMYSQL
+echo $i table truncated in $line database
+# mysqlcheck --port=3306 -h localhost -r -u $DATABASEUSER --password=$DATABASEPASS $line &> /dev/null
+# mysqlcheck --port=3306 -h localhost -o -u $DATABASEUSER --password=$DATABASEPASS $line &> /dev/null
+done
+}
+
 #Check if location to save exists, if not create it
 [ ! -a $SAVELOCATION ] &&  mkdir -p $SAVELOCATION ;
 
-#Get a list of datbases to backup
+#Get a list of databases to backup
 mysql -u $DATABASEUSER -p$DATABASEPASS -e "show databases" -s > .databasesToBackup;
 
-#Parese list of databases and then backup using mysqldump
+#Parse the list of databases and then backup using mysqldump
 cat .databasesToBackup | while read line; do
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE sessions;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE cache;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE cache_menu;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE cache_page;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE cache_filter;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE cache_form;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE cache_block;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE cache_views_data;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE cache_views;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE cache_content;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE watchdog;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE cache_path;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE boost_cache;
-EOFMYSQL
-mysql --default-character-set=utf8 --password=$DATABASEPASS -h localhost --port=3306 -u $DATABASEUSER $line<<EOFMYSQL
-TRUNCATE boost_cache_relationships;
-EOFMYSQL
-# mysqlcheck --port=3306 -h localhost -r -u $DATABASEUSER --password=$DATABASEPASS $line &> /dev/null
-# mysqlcheck --port=3306 -h localhost -o -u $DATABASEUSER --password=$DATABASEPASS $line &> /dev/null
-mysqldump -u $DATABASEUSER -p$DATABASEPASS --default-character-set=utf8 -Q -C -e --hex-blob --add-drop-table $line | gzip  > $SAVELOCATION/$line.sql.gz
-sleep 1
+  truncate_cache_tables
+  mysqldump -u $DATABASEUSER -p$DATABASEPASS --default-character-set=utf8 -Q -C -e --hex-blob --add-drop-table $line | gzip  > $SAVELOCATION/$line.sql.gz
+  echo backup completed for $line database
+  sleep 1
 done
 
 rm .databasesToBackup
 
-#Delete all files in the backup dir 8 days or older - note this deletes everything!
-#only database backups should exist in $BACKUPDIR!!!
+#Delete all files in the backup dir 8 days or older - note: this deletes everything!
+#Only database backups should exist in $BACKUPDIR!!!
 find $BACKUPDIR -mtime +8 -type d -exec rm -rf {} \;
 
 echo COMPLETED ALL
