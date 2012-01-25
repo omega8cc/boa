6ac639a4481c8d6d33fbbea50920137c3f448f84
Barracuda Team <admin@omega8.cc>
2012-01-25 04:11:10 +0100

    BOA: Small improvements to load monitor and some Nginx settings (Robert)

--- aegir/tools/auto-healing-monitoring/second.sh
+++ aegir/tools/auto-healing-monitoring/second.sh
@@ -9,12 +9,16 @@ hold()
   /etc/init.d/php-fpm stop
   /etc/init.d/redis-server stop
   killall -9 memcached php-fpm php-cgi
+  echo load is $ONEX_LOAD:$FIVX_LOAD while maxload is $CTL_ONEX_LOAD:$CTL_FIVX_LOAD
+}
+
+terminate()
+{
   if test -f /var/run/octopus_barracuda.pid ; then
     sleep 1
   else
     killall -9 php wget
   fi
-  echo load is $ONEX_LOAD:$FIVX_LOAD while maxload is $CTL_ONEX_LOAD:$CTL_FIVX_LOAD
 }
 
 control()
@@ -23,6 +27,13 @@ ONEX_LOAD=`awk '{print $1*100}' /proc/loadavg`
 FIVX_LOAD=`awk '{print $2*100}' /proc/loadavg`
 CTL_ONEX_LOAD=888
 CTL_FIVX_LOAD=555
+CTL_ONEX_LOAD_CRIT=1900
+CTL_FIVX_LOAD_CRIT=1400
+if [ $ONEX_LOAD -gt $CTL_ONEX_LOAD_CRIT ]; then
+  terminate
+elif [ $FIVX_LOAD -gt $CTL_FIVX_LOAD_CRIT ]; then
+  terminate
+fi
 if [ $ONEX_LOAD -gt $CTL_ONEX_LOAD ]; then
   hold
 elif [ $FIVX_LOAD -gt $CTL_FIVX_LOAD ]; then
