25b6c6844b5263719ebc46eecc04110735fb44b6
Barracuda Team <admin@omega8.cc>
2012-03-04 15:49:42 +0100

    BOA: Cleanup #1 (Robert)

---
+++ aegir/tools/auto-healing-monitoring/purge_cruft.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+find /data/disk/*/backups/* -mtime +1 -type f -exec rm -rf {} \;
+find /data/disk/*/distro/*/*/sites/*/files/backup_migrate/*/* -mtime +1 -type f -exec rm -rf {} \;
+find /data/disk/*/distro/*/*/sites/*/files/tmp/* -mtime +1 -type f -exec rm -rf {} \;
+find /data/disk/*/distro/*/*/sites/*/private/files/backup_migrate/*/* -mtime +1 -type f -exec rm -rf {} \;
+find /data/disk/*/static/*/sites/*/files/backup_migrate/*/* -mtime +1 -type f -exec rm -rf {} \;
+find /data/disk/*/static/*/sites/*/files/tmp/* -mtime +1 -type f -exec rm -rf {} \;
+rm -f /var/backups/dragon/x/xdrago*/log/VISITOR_ABUSE_ONE.log
