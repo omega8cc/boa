0283ea9ccdd6f371994bb3693ff368bf23818d39
Barracuda Team <admin@omega8.cc>
2012-03-24 12:59:50 +0100

    BOA: Fix for PATH (Robert)

--- aegir/tools/system/runner.sh
+++ aegir/tools/system/runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SHELL=/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
 ###-------------SYSTEM-----------------###
 
