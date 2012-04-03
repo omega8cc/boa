22185f58a525634ad987f8c3512d648684973268
Barracuda Team <admin@omega8.cc>
2012-04-03 17:09:14 +0200

    BOA: Nginx full restart is not required (Robert)

--- aegir/tools/system/graceful.sh
+++ aegir/tools/system/graceful.sh
@@ -15,26 +15,16 @@ action()
   rm -f /opt/tmp/*error*
   rm -f /opt/tomcat6/logs/*
   rm -f -r /var/lib/nginx/speed/*
-  /etc/init.d/nginx stop
-  /etc/init.d/php-fpm stop
-  /etc/init.d/php53-fpm stop
-  killall -9 php-fpm php-cgi nginx php wget
-  sleep 1
-  killall -9 php-fpm php-cgi nginx php wget
   echo rotate > /var/log/nginx/speed_purge.log
-  /etc/init.d/php-fpm start
-  /etc/init.d/php53-fpm start
-  /etc/init.d/nginx start
-  rm -f /var/xdrago/log/wait-for-octopus-barracuda-running
+  /etc/init.d/nginx reload
   touch /var/xdrago/log/graceful.done
 }
 
 if test -f /var/run/boa_run.pid ; then
-  touch /var/xdrago/log/wait-for-octopus-barracuda-running
   exit
 else
   touch /var/run/boa_wait.pid
-  sleep 300
+  sleep 60
   action
   rm -f /var/run/boa_wait.pid
 fi
