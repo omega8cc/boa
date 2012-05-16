ef2798f9b42d2fcf33a538e07ba6833f5d7d69d7
Barracuda Team <admin@omega8.cc>
2012-05-16 20:56:09 +0200

    BOA: Set hosting_queue_cron_frequency to 8888 weeks by default to really use schedule defined via hosting_advanced_cron module and never override it (Robert)

--- aegir/tools/system/usage.sh
+++ aegir/tools/system/usage.sh
@@ -424,7 +424,7 @@ do
         cd $_THIS_HM_SITE
         su -s /bin/bash $_THIS_HM_USER -c "drush vset --always-set site_footer 'Daily Usage Monitor | Disk <strong>$HomSizH</strong> MB | Databases <strong>$SumDatH</strong> MB | <strong>$_CLIENT_CORES</strong> C' &> /dev/null"
         if [ ! -e "$User/log/custom_cron" ] ; then
-          su -s /bin/bash $_THIS_HM_USER -c "drush vset --always-set hosting_queue_cron_frequency 3600 &> /dev/null"
+          su -s /bin/bash $_THIS_HM_USER -c "drush vset --always-set hosting_queue_cron_frequency 5375462400 &> /dev/null"
           su -s /bin/bash $_THIS_HM_USER -c "drush vset --always-set hosting_cron_use_backend 1 &> /dev/null"
         fi
       fi
