445bf09f324fa270623149ddff2c0a2993e9155f
Barracuda Team <admin@omega8.cc>
2012-05-21 04:13:40 +0200

    BOA: Issue #1591746 by omega8cc - Update GeoIP.dat file automatically (Grace)

--- aegir/tools/system/graceful.sh
+++ aegir/tools/system/graceful.sh
@@ -5,6 +5,17 @@ PATH=/usr/local/sbin:/usr/local/bin:/opt/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 action()
 {
+  mkdir -p /usr/share/GeoIP
+  chmod 755 /usr/share/GeoIP
+  cd /tmp
+  wget -q -U iCab http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+  gunzip GeoIP.dat.gz &> /dev/null
+  cp -af GeoIP.dat /usr/share/GeoIP/
+  wget -q -U iCab http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz
+  gunzip GeoIPv6.dat.gz &> /dev/null
+  cp -af GeoIPv6.dat /usr/share/GeoIP/
+  chmod 644 /usr/share/GeoIP/*
+  rm -f -r /tmp/GeoIP*
   rm -f -r /opt/tmp
   mkdir -p /opt/tmp
   chmod 777 /opt/tmp
