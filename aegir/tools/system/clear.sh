a852f1cda654605b7d31ecaca4fb50b199419228
Barracuda Team <admin@omega8.cc>
2012-11-04 15:32:52 +0100

    Change the order of commands to always log results of last reload/restart.

--- aegir/tools/system/clear.sh
+++ aegir/tools/system/clear.sh
@@ -3,26 +3,6 @@
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/opt/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-if test -f /root/.high_traffic.cnf ; then
-  echo rotate > /var/log/nginx/access.log
-else
-  /etc/init.d/redis-server stop
-  sleep 1
-  killall -9 redis-server
-  rm -f /var/run/redis.pid
-  rm -f /var/lib/redis/*
-  sleep 1
-  /etc/init.d/redis-server start
-  if test -f /etc/init.d/tomcat ; then
-    /etc/init.d/tomcat stop
-    sleep 3
-    killall -9 java
-  fi
-  /etc/init.d/php53-fpm reload
-  if test -f /etc/init.d/php-fpm ; then
-    /etc/init.d/php-fpm reload
-  fi
-fi
 echo rotate > /var/log/php/php-fpm-error.log
 echo rotate > /var/log/php/php-fpm-slow.log
 echo rotate > /var/log/php/php53-fpm-error.log
@@ -33,6 +13,26 @@ echo rotate > /var/log/php/error_log_cli_52
 echo rotate > /var/log/php/error_log_cli_53
 echo rotate > /var/log/redis/redis-server.log
 echo rotate > /var/log/mysql/sql-slow-query.log
+if test -f /root/.high_traffic.cnf ; then
+  echo rotate > /var/log/nginx/access.log
+else
+  /etc/init.d/redis-server stop
+  sleep 1
+  killall -9 redis-server
+  rm -f /var/run/redis.pid
+  rm -f /var/lib/redis/*
+  sleep 1
+  /etc/init.d/redis-server start
+  /etc/init.d/php53-fpm reload
+  if test -f /etc/init.d/php-fpm ; then
+    /etc/init.d/php-fpm reload
+  fi
+  if test -f /etc/init.d/tomcat ; then
+    /etc/init.d/tomcat stop
+    sleep 3
+    killall -9 java
+  fi
+fi
 if test -f /var/run/boa_run.pid ; then
   sleep 1
 else
