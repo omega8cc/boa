7f1b7f1b204146322bfc48242868382181cde795
Barracuda Team <admin@omega8.cc>
2012-11-11 02:05:14 +0100

    Fix indentation.

--- aegir/tools/system/usage.sh
+++ aegir/tools/system/usage.sh
@@ -177,16 +177,16 @@ if [ "$_MODULES" = "YES" ] ; then
     if [ -e "$Dir/drushrc.php" ] ; then
       cd $Dir
       if [ -e "$Plr/profiles/hostmaster" ] && [ ! -f "$Plr/profiles/hostmaster/modules-fix.txt" ] ; then
-    su -s /bin/bash - $_THIS_HM_USER -c "drush dis cache syslog dblog -y &> /dev/null"
-    echo "modules-fixed" > $Plr/profiles/hostmaster/modules-fix.txt
-    chown $_THIS_HM_USER:users $Plr/profiles/hostmaster/modules-fix.txt
+        su -s /bin/bash - $_THIS_HM_USER -c "drush dis cache syslog dblog -y &> /dev/null"
+        echo "modules-fixed" > $Plr/profiles/hostmaster/modules-fix.txt
+        chown $_THIS_HM_USER:users $Plr/profiles/hostmaster/modules-fix.txt
       elif [ -e "$Plr/modules/o_contrib" ] ; then
-    su -s /bin/bash - $_THIS_HM_USER -c "drush dis $_MODULES_OFF_SIX -y &> /dev/null"
-    su -s /bin/bash - $_THIS_HM_USER -c "drush en $_MODULES_ON_SIX -y &> /dev/null"
-    su -s /bin/bash - $_THIS_HM_USER -c "drush sqlq \"UPDATE system SET weight = '-1' WHERE type = 'module' AND name = 'path_alias_cache'\" &> /dev/null"
+        su -s /bin/bash - $_THIS_HM_USER -c "drush dis $_MODULES_OFF_SIX -y &> /dev/null"
+        su -s /bin/bash - $_THIS_HM_USER -c "drush en $_MODULES_ON_SIX -y &> /dev/null"
+        su -s /bin/bash - $_THIS_HM_USER -c "drush sqlq \"UPDATE system SET weight = '-1' WHERE type = 'module' AND name = 'path_alias_cache'\" &> /dev/null"
       elif [ -e "$Plr/modules/o_contrib_seven" ] ; then
-    su -s /bin/bash - $_THIS_HM_USER -c "drush dis $_MODULES_OFF_SEVEN -y &> /dev/null"
-    su -s /bin/bash - $_THIS_HM_USER -c "drush en $_MODULES_ON_SEVEN -y &> /dev/null"
+        su -s /bin/bash - $_THIS_HM_USER -c "drush dis $_MODULES_OFF_SEVEN -y &> /dev/null"
+        su -s /bin/bash - $_THIS_HM_USER -c "drush en $_MODULES_ON_SEVEN -y &> /dev/null"
       fi
     fi
     ;;
