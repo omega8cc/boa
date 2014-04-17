787b1bbcf207a1a0e4b3f55ce8199f05f223f1eb
Barracuda Team <admin@omega8.cc>
2014-04-17 16:13:50 +0200

    Add /opt/local/bin to PATH by default.

--- aegir/tools/system/minute.sh
+++ aegir/tools/system/minute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SHELL=/bin/bash
-PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
+PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/usr/bin:/usr/sbin:/bin:/sbin
 
 if [ -e "/root/.high_traffic.cnf" ] ; then
   _DO_NOTHING=YES
