06b6c24255be4ce3d29fbb258a4f32f33f9f2343
Barracuda Team <admin@omega8.cc>
2014-04-26 17:10:07 +0200

    PHP: Sync incorrect default_socket_timeout (60s) with max_execution_time (180s)

--- aegir/tools/system/manage_ltd_users.sh
+++ aegir/tools/system/manage_ltd_users.sh
@@ -591,6 +591,9 @@ switch_php()
           if [ "$_PHP_FPM_TIMEOUT" -lt "60" ] ; then
             _PHP_FPM_TIMEOUT=60
           fi
+          if [ "$_PHP_FPM_TIMEOUT" -gt "180" ] ; then
+            _PHP_FPM_TIMEOUT=180
+          fi
           if [ ! -z "$_PHP_FPM_TIMEOUT" ] ; then
             _PHP_TO="${_PHP_FPM_TIMEOUT}s"
             sed -i "s/180s/$_PHP_TO/g" /opt/php${_PHP_SV}/etc/pool.d/${_OWN}.conf &> /dev/null
