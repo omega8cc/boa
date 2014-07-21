e955a4577d9611cb03cd33358bda08c12267c5d6
Barracuda Team <admin@omega8.cc>
2014-07-21 23:36:35 +0200

    Update for Contrib [F]orce[D]isabled modules list.

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -9,8 +9,8 @@ PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/usr/bin:/usr/sbin:/bin:/sbin
 
 _MODULES_ON_SEVEN="robotstxt"
 _MODULES_ON_SIX="path_alias_cache robotstxt"
-_MODULES_OFF_SEVEN="background_process coder dblog devel hacked l10n_update performance syslog ultimate_cron update memcache memcache_admin varnish watchdog_live"
-_MODULES_OFF_SIX="background_process coder cookie_cache_bypass css_gzip dblog devel javascript_aggregator hacked l10n_update performance poormanscron supercron syslog ultimate_cron update memcache memcache_admin varnish watchdog_live"
+_MODULES_OFF_SEVEN="background_process coder dblog devel hacked l10n_update memcache memcache_admin performance search_krumo security_review site_audit stage_file_proxy syslog ultimate_cron update varnish watchdog_live xhprof"
+_MODULES_OFF_SIX="background_process coder cookie_cache_bypass css_gzip dblog devel hacked javascript_aggregator l10n_update memcache memcache_admin performance poormanscron search_krumo security_review stage_file_proxy supercron syslog ultimate_cron update varnish watchdog_live xhprof"
 
 ###-------------SYSTEM-----------------###
 
