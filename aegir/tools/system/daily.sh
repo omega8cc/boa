bcc18c82d1c1dde645c78901aac314310ffb7761
Barracuda Team <admin@omega8.cc>
2014-08-09 01:37:07 +0200

    Improve cleanup for empty platforms directories (2).

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -1350,15 +1350,15 @@ purge_cruft_machine () {
 
   REVISIONS="001 002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018 019 020 021 022 023 024 025 026 027 028 029 030 031 032 033 034 035 036 037 038 039 040 041 042 043 044 045 046 047 048 049 050"
   for i in $REVISIONS; do
-    if [ -e "/home/${_THIS_HM_USER}.ftp/platforms/$i/keys" ] ; then
+    if [ -e "/home/${_THIS_HM_USER}.ftp/platforms/$i" ] ; then
       RevisionTest=$(ls /home/${_THIS_HM_USER}.ftp/platforms/$i | wc -l | tr -d "\n" 2>&1)
-      if [ "$RevisionTest" = "1" ] && [ ! -z "$RevisionTest" ] ; then
+      if [ "$RevisionTest" -lt "2" ] && [ ! -z "$RevisionTest" ] ; then
         rm -f -r /home/${_THIS_HM_USER}.ftp/platforms/$i
       fi
     fi
-    if [ -d "$User/distro/$i/keys" ] ; then
+    if [ -d "$User/distro/$i" ] ; then
       RevisionTest=$(ls $User/distro/$i | wc -l | tr -d "\n" 2>&1)
-      if [ "$RevisionTest" = "1" ] && [ ! -z "$RevisionTest" ] ; then
+      if [ "$RevisionTest" -lt "2" ] && [ ! -z "$RevisionTest" ] ; then
         mkdir -p $User/undo
         mv -f $User/distro/$i $User/undo/ &> /dev/null
       fi
