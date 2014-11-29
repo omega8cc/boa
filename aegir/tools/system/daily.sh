c5d51db0832188cfba68d15686028f1cdf205f71
Barracuda Team <admin@omega8.cc>
2014-11-29 03:37:38 +0100

    Restore defaults.

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -1871,7 +1871,7 @@ check_old_empty_platforms () {
         _DO_NOTHING=YES
       else
         if [[ "$_HOST_TEST" =~ ".host8." ]] || [ "$_VMFAMILY" = "VS" ] ; then
-          _DEL_OLD_EMPTY_PLATFORMS="5"
+          _DEL_OLD_EMPTY_PLATFORMS="30"
         else
           _DEL_OLD_EMPTY_PLATFORMS="60"
         fi
