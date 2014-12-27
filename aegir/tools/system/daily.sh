e5932191ab1470e8732970920bb8a6ac3b472501
Barracuda Team <admin@omega8.cc>
2014-12-27 04:11:34 +0100

    Update overrides for CI instances.

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -1864,7 +1864,7 @@ delete_this_platform () {
 
 check_old_empty_platforms () {
   if [[ "$_HOST_TEST" =~ ".host8." ]] || [ "$_VMFAMILY" = "VS" ] || [ -e "/root/.host8.cnf" ] ; then
-    if [[ "$_HOST_TEST" =~ "v189q.nyc." ]] || [[ "$_HOST_TEST" =~ ".qq.o8.io" ]] ; then
+    if [[ "$_HOST_TEST" =~ "v189q.nyc." ]] || [ -e "/root/.debug.cnf" ] ; then
       _DO_NOTHING=YES
     else
       if [ "$_DEL_OLD_EMPTY_PLATFORMS" -gt "0" ] && [ ! -z "$_DEL_OLD_EMPTY_PLATFORMS" ] ; then
