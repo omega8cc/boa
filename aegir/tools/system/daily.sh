fc44385c02dc9574ec2133446828fb247ae664db
Barracuda Team <admin@omega8.cc>
2015-01-17 04:14:53 +0100

    It is still 2.4.0-dev

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -4,6 +4,7 @@ SHELL=/bin/bash
 PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/usr/bin:/usr/sbin:/bin:/sbin
 _WEBG=www-data
 _OPENSSL_VRN=1.0.1j
+_X_SE="2.3.8-stable"
 _OSV=$(lsb_release -sc 2>&1)
 _SSL_ITD=$(openssl version 2>&1 \
   | tr -d "\n" \
@@ -804,7 +805,7 @@ update_solr() {
   # $1 is module
   # $2 is solr core path
   if [ ! -z "$1" ] \
-    && [ ! -e "$2/conf/BOA-2.4.0.conf" ] \
+    && [ ! -e "$2/conf/${_X_SE}.conf" ] \
     && [ -e "/var/xdrago/conf/solr" ] \
     && [ -e "$2/conf" ]; then
     if [ "$1" = "apachesolr" ]; then
@@ -829,7 +830,7 @@ update_solr() {
     if [ -e "$2/conf/update-ok.txt" ]; then
       write_solr_config $1 ${Dir}/solr.php
       echo "Updated Solr with $1 for $2"
-      touch $2/conf/BOA-2.4.0.conf
+      touch $2/conf/${_X_SE}.conf
       if [ -e "/etc/default/jetty9" ] && [ -e "/etc/init.d/jetty9" ]; then
         kill -9 $(ps aux | grep '[j]etty9' | awk '{print $2}') &> /dev/null
         service jetty9 start &> /dev/null
@@ -975,7 +976,7 @@ setup_solr() {
       ${_DIR_CTRL_FILE} 2>&1)
     if [[ "$_SOLR_UPDATE_CONFIG_TEST" =~ "solr_update_config = YES" ]]; then
       if [ "$_SOLR_CUSTOM_CONFIG_RESULT" = "YES" ] \
-        || [ -e "${_SOLR_DIR}/conf/BOA-2.4.0.conf" ]; then
+        || [ -e "${_SOLR_DIR}/conf/${_X_SE}.conf" ]; then
         _DO_NOTHING=YES
       else
         update_solr ${_SOLR_MODULE} ${_SOLR_DIR}
@@ -1927,7 +1928,7 @@ add_note_platform_ini() {
     echo ";;" >> $_CTRL_FILE
     echo ";;  Please review complete documentation included in this file TEMPLATE:"     >> $_CTRL_FILE
     echo ";;  default.boa_platform_control.ini, since this ACTIVE INI file"             >> $_CTRL_FILE
-    echo ";;  may not include all options available after upgrade to BOA-2.4.0"         >> $_CTRL_FILE
+    echo ";;  may not include all options available after upgrade to BOA-${_X_SE}"      >> $_CTRL_FILE
     echo ";;" >> $_CTRL_FILE
     echo ";;  Note that it takes ~60 seconds to see any modification results in action" >> $_CTRL_FILE
     echo ";;  due to opcode caching enabled in PHP-FPM for all non-dev sites."          >> $_CTRL_FILE
@@ -1944,7 +1945,7 @@ add_note_site_ini() {
     echo ";;" >> $_CTRL_FILE
     echo ";;  Please review complete documentation included in this file TEMPLATE:"     >> $_CTRL_FILE
     echo ";;  default.boa_site_control.ini, since this ACTIVE INI file"                 >> $_CTRL_FILE
-    echo ";;  may not include all options available after upgrade to BOA-2.4.0"         >> $_CTRL_FILE
+    echo ";;  may not include all options available after upgrade to BOA-${_X_SE}"      >> $_CTRL_FILE
     echo ";;" >> $_CTRL_FILE
     echo ";;  Note that it takes ~60 seconds to see any modification results in action" >> $_CTRL_FILE
     echo ";;  due to opcode caching enabled in PHP-FPM for all non-dev sites."          >> $_CTRL_FILE
@@ -2671,24 +2672,24 @@ else
   if [ -e "/data/all" ]; then
     find /data/all -type f -name "*.info" -print0 | xargs -0 sed -i \
       's/.*dependencies\[\] = update/;dependencies\[\] = update/g' &> /dev/null
-    if [ ! -e "/data/all/permissions-fix-post-up-BOA-2.4.0.info" ]; then
+    if [ ! -e "/data/all/permissions-fix-post-up-${_X_SE}.info" ]; then
       rm -f /data/all/permissions-fix*
       find /data/disk/*/distro/*/*/sites/all/{libraries,modules,themes} \
         -type d -exec chmod 02775 {} \; &> /dev/null
       find /data/disk/*/distro/*/*/sites/all/{libraries,modules,themes} \
         -type f -exec chmod 0664 {} \; &> /dev/null
-      echo fixed > /data/all/permissions-fix-post-up-BOA-2.4.0.info
+      echo fixed > /data/all/permissions-fix-post-up-${_X_SE}.info
     fi
   elif [ -e "/data/disk/all" ]; then
     find /data/disk/all -type f -name "*.info" -print0 | xargs -0 sed -i \
       's/.*dependencies\[\] = update/;dependencies\[\] = update/g' &> /dev/null
-    if [ ! -e "/data/disk/all/permissions-fix-post-up-BOA-2.4.0.info" ]; then
+    if [ ! -e "/data/disk/all/permissions-fix-post-up-${_X_SE}.info" ]; then
       rm -f /data/disk/all/permissions-fix*
       find /data/disk/*/distro/*/*/sites/all/{libraries,modules,themes} \
         -type d -exec chmod 02775 {} \; &> /dev/null
       find /data/disk/*/distro/*/*/sites/all/{libraries,modules,themes} \
         -type f -exec chmod 0664 {} \; &> /dev/null
-      echo fixed > /data/disk/all/permissions-fix-post-up-BOA-2.4.0.info
+      echo fixed > /data/disk/all/permissions-fix-post-up-${_X_SE}.info
     fi
   fi
   action >/var/xdrago/log/daily/daily-${_NOW}.log 2>&1
@@ -2784,11 +2785,11 @@ if [ "${_PERMISSIONS_FIX}" = "YES" ] \
   chmod 02775 /data/disk/*/distro/*/*/sites/all/{modules,libraries,themes} &> /dev/null
   echo fixed > /data/all/permissions-fix-${_X_VERSION}-fixed-dz.info
 fi
-if [ ! -e "/var/backups/fix-sites-all-permsissions-2.4.0.txt" ]; then
+if [ ! -e "/var/backups/fix-sites-all-permsissions-${_X_SE}.txt" ]; then
   chmod 0751  /data/disk/*/distro/*/*/sites
   chmod 0755  /data/disk/*/distro/*/*/sites/all
   chmod 02775 /data/disk/*/distro/*/*/sites/all/{modules,libraries,themes}
-  echo FIXED > /var/backups/fix-sites-all-permsissions-2.4.0.txt
+  echo FIXED > /var/backups/fix-sites-all-permsissions-${_X_SE}.txt
   echo "Permissions in sites/all tree just fixed"
 fi
 if [ ! -e "/root/.upstart.cnf" ]; then
