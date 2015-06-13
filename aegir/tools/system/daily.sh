3d86c6e400cf5e7bc7aca1f446a5579a393e15e3
Barracuda Team <admin@omega8.cc>
2015-06-13 14:13:56 +0200

    OpenSSL 1.0.2c (if installed from sources)

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -3,7 +3,7 @@
 SHELL=/bin/bash
 PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/usr/bin:/usr/sbin:/bin:/sbin
 _WEBG=www-data
-_OPENSSL_VRN=1.0.2a
+_OPENSSL_VRN=1.0.2c
 _X_SE="2.4.3-stable"
 _OSV=$(lsb_release -sc 2>&1)
 _SSL_ITD=$(openssl version 2>&1 \
