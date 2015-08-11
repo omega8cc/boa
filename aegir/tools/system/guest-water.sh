43b11c0a3e434a481902cdded0618cd7df39971b
Barracuda Team <admin@omega8.cc>
2015-08-11 14:48:49 +0200

    User short sleep on firewall temp blocks cleanup

--- aegir/tools/system/guest-water.sh
+++ aegir/tools/system/guest-water.sh
@@ -167,7 +167,7 @@ if [ -e "/etc/csf/csf.deny" ] && [ -e "/usr/sbin/csf" ]; then
   echo Waiting $n seconds...
   sleep $n
   touch /var/run/water.pid
-  sleep 180
+  sleep 10
   local_ip_rg
   _HA=/var/xdrago/monitor/hackcheck.archive.log
   _WA=/var/xdrago/monitor/scan_nginx.archive.log
