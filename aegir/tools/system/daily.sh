8a4387863601d39e9871bf8fcaab0392c78ca959
Barracuda Team <admin@omega8.cc>
2015-09-19 20:31:33 +0200

    Stable BOA-2.4.6 Release - Full Edition

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -4,7 +4,7 @@ SHELL=/bin/bash
 PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/usr/bin:/usr/sbin:/bin:/sbin
 _WEBG=www-data
 _OPENSSL_VRN=1.0.1p
-_X_SE="2.4.5-stable"
+_X_SE="2.4.6-stable"
 _OSV=$(lsb_release -sc 2>&1)
 _SSL_ITD=$(openssl version 2>&1 \
   | tr -d "\n" \
