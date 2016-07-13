c7efe35fe9bcdb3aabf677121db112130b1e5bbd
Barracuda Team <admin@omega8.cc>
2016-07-13 19:04:54 +0200

    Enable debug msg

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -253,7 +253,7 @@ check_if_force() {
   for s in ${_MODULES_FORCE}; do
     if [ ! -z "$1" ] && [ "$s" = "$1" ]; then
       _FORCE=YES
-      #echo $1 is blacklisted and will be forcefully disabled in ${Dom}
+      echo $1 is blacklisted and will be forcefully disabled in ${Dom}
     fi
   done
 }
