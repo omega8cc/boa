594b6b12cad6a5a6b76452bd4bb766e2be816f9c
Barracuda Team <admin@omega8.cc>
2017-02-12 10:44:45 +0100

    Use 314stableQ7.pid

--- aegir/tools/system/manage_ltd_users.sh
+++ aegir/tools/system/manage_ltd_users.sh
@@ -153,7 +153,7 @@ enable_chattr() {
     _U_HD="/home/$1/.drush"
     _U_TP="/home/$1/.tmp"
     _U_II="${_U_HD}/php.ini"
-    if [ ! -e "${_U_HD}/.ctrl.314stableQ6.pid" ]; then
+    if [ ! -e "${_U_HD}/.ctrl.314stableQ7.pid" ]; then
       if [[ "${_CHECK_HOST}" =~ ".host8." ]] \
         || [[ "${_CHECK_HOST}" =~ ".boa.io" ]] \
         || [ "${_VMFAMILY}" = "VS" ]; then
@@ -221,7 +221,7 @@ enable_chattr() {
 
     if [ "${_PHP_CLI_UPDATE}" = "YES" ] \
       || [ ! -e "${_U_II}" ] \
-      || [ ! -e "${_U_HD}/.ctrl.314stableQ6.pid" ]; then
+      || [ ! -e "${_U_HD}/.ctrl.314stableQ7.pid" ]; then
       mkdir -p ${_U_HD}
       rm -f ${_U_HD}/.ctrl.php*
       rm -f ${_U_II}
@@ -301,7 +301,7 @@ enable_chattr() {
         sed -i "s/.*upload_tmp_dir =.*/upload_tmp_dir = ${_QTP}/g"           ${_U_II}
         wait
         echo > ${_U_HD}/.ctrl.php${_U_INI}.pid
-        echo > ${_U_HD}/.ctrl.314stableQ6.pid
+        echo > ${_U_HD}/.ctrl.314stableQ7.pid
       fi
     fi
 
@@ -810,7 +810,7 @@ update_php_cli_local_ini() {
   if [ "${_PHP_CLI_UPDATE}" = "YES" ] \
     || [ ! -e "${_U_II}" ] \
     || [ ! -d "${_U_TP}" ] \
-    || [ ! -e "${_U_HD}/.ctrl.314stableQ6.pid" ]; then
+    || [ ! -e "${_U_HD}/.ctrl.314stableQ7.pid" ]; then
     mkdir -p ${_U_TP}
     touch ${_U_TP}
     find ${_U_TP}/ -mtime +0 -exec rm -rf {} \; &> /dev/null
@@ -874,7 +874,7 @@ update_php_cli_local_ini() {
       sed -i "s/.*upload_tmp_dir =.*/upload_tmp_dir = ${_QTP}/g"           ${_U_II}
       wait
       echo > ${_U_HD}/.ctrl.php${_U_INI}.pid
-      echo > ${_U_HD}/.ctrl.314stableQ6.pid
+      echo > ${_U_HD}/.ctrl.314stableQ7.pid
     fi
     chattr +i ${_U_II}
   fi
@@ -1842,16 +1842,16 @@ manage_user() {
         -type d -exec chmod 0700 {} \; &> /dev/null
       find ${dscUsr}/config/server_master \
         -type f -exec chmod 0600 {} \; &> /dev/null
-      if [ ! -e "${dscUsr}/.tmp/.ctrl.314stableQ6.pid" ]; then
+      if [ ! -e "${dscUsr}/.tmp/.ctrl.314stableQ7.pid" ]; then
         rm -rf ${dscUsr}/.drush/cache
         mkdir -p ${dscUsr}/.tmp
         touch ${dscUsr}/.tmp
         find ${dscUsr}/.tmp/ -mtime +0 -exec rm -rf {} \; &> /dev/null
         chown ${_USER}:${usrGroup} ${dscUsr}/.tmp &> /dev/null
         chmod 02755 ${dscUsr}/.tmp &> /dev/null
-        echo OK > ${dscUsr}/.tmp/.ctrl.314stableQ6.pid
+        echo OK > ${dscUsr}/.tmp/.ctrl.314stableQ7.pid
       fi
-      if [ ! -e "${dscUsr}/static/control/.ctrl.314stableQ6.pid" ]; then
+      if [ ! -e "${dscUsr}/static/control/.ctrl.314stableQ7.pid" ]; then
         mkdir -p ${dscUsr}/static/control
         chmod 755 ${dscUsr}/static/control
         if [ -e "/var/xdrago/conf/control-readme.txt" ]; then
@@ -1862,7 +1862,7 @@ manage_user() {
         chown -R ${_USER}.ftp:${usrGroup} \
           ${dscUsr}/static/control &> /dev/null
         rm -f ${dscUsr}/static/control/.ctrl.*
-        echo OK > ${dscUsr}/static/control/.ctrl.314stableQ6.pid
+        echo OK > ${dscUsr}/static/control/.ctrl.314stableQ7.pid
       fi
       if [ -e "${dscUsr}/static/control/ssl-live-mode.info" ]; then
         if [ -e "${dscUsr}/tools/le/.ctrl/ssl-demo-mode.pid" ]; then
@@ -1940,13 +1940,13 @@ manage_user() {
             ln -sf ${dscUsr}/clients /home/${_USER}.ftp/clients
             ln -sf ${dscUsr}/static  /home/${_USER}.ftp/static
           fi
-          if [ ! -e "/home/${_USER}.ftp/.tmp/.ctrl.314stableQ6.pid" ]; then
+          if [ ! -e "/home/${_USER}.ftp/.tmp/.ctrl.314stableQ7.pid" ]; then
             rm -rf /home/${_USER}.ftp/.drush/cache
             rm -rf /home/${_USER}.ftp/.tmp
             mkdir -p /home/${_USER}.ftp/.tmp
             chown ${_USER}.ftp:${usrGroup} /home/${_USER}.ftp/.tmp &> /dev/null
             chmod 700 /home/${_USER}.ftp/.tmp &> /dev/null
-            echo OK > /home/${_USER}.ftp/.tmp/.ctrl.314stableQ6.pid
+            echo OK > /home/${_USER}.ftp/.tmp/.ctrl.314stableQ7.pid
           fi
           enable_chattr ${_USER}.ftp
           echo Done for ${pthParentUsr}
@@ -1964,7 +1964,7 @@ manage_user() {
 
 ###-------------SYSTEM-----------------###
 
-if [ ! -e "/home/.ctrl.314stableQ6.pid" ]; then
+if [ ! -e "/home/.ctrl.314stableQ7.pid" ]; then
   chattr -i /home
   chmod 0711 /home
   chown root:root /home
@@ -1994,7 +1994,7 @@ if [ ! -e "/home/.ctrl.314stableQ6.pid" ]; then
       fi
     fi
   done < /etc/passwd
-  touch /home/.ctrl.314stableQ6.pid
+  touch /home/.ctrl.314stableQ7.pid
 fi
 
 if [ ! -L "/usr/bin/MySecureShell" ] && [ -x "/usr/bin/mysecureshell" ]; then
