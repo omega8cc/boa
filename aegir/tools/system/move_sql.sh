367d484dc135c737184ecfcb1e78b8e39904eb03
Barracuda Team <admin@omega8.cc>
2017-02-28 00:05:05 +0100

    Use absolutely graceful MySQLD restart procedure
    On some big servers with hundreds of sites and/or many GB of data the mysqld restart can easily go wrong, with conflicting instances started, each other trying to lock bin logs, which translates to extended downtime, very slow recovery and very possible little single-table crashes preventing the server from starting properly. This should no longer happen with BOA enhanced procedure, but Debian packages can still cause the same problems on version upgrade, so we should cleanly shutdown mysqld if upcoming upgrade is detected.

--- aegir/tools/system/move_sql.sh
+++ aegir/tools/system/move_sql.sh
@@ -11,34 +11,94 @@ if [ -z "${_B_NICE}" ]; then
   _B_NICE=10
 fi
 
-if [ -e "/var/xdrago/log/mysql_restart_running.pid" ]; then
-  touch /var/xdrago/log/wait-for-mysql-restart
-  echo "mysql server restart already in progress..."
-  exit 0
+if [ -e "/var/run/mysql_restart_running.pid" ]; then
+  echo "MySQLD restart procedure already in progress, it seems?"
+  echo "Nothing to do, let's quit now. Bye!"
+  exit 1
 else
+  echo "Creating locks.."
   touch /var/run/boa_wait.pid
-  touch /var/xdrago/log/mysql_restart_running.pid
-  if [ "${_DB_SERIES}" = "10.1" ]; then
-    mysql -u root -e "SET GLOBAL innodb_max_dirty_pages_pct = 0;" &> /dev/null
-    mysql -u root -e "SET GLOBAL innodb_buffer_pool_dump_at_shutdown = 1;" &> /dev/null
-    mysql -u root -e "SET GLOBAL innodb_io_capacity = 8000;" &> /dev/null
-    mysql -u root -e "SET GLOBAL innodb_buffer_pool_dump_pct = 100;" &> /dev/null
+  touch /var/run/mysql_restart_running.pid
+  touch /var/run/fmp_wait.pid
+
+  echo "Stopping Nginx now.."
+  service nginx stop &> /dev/null
+  until [ -z "${_IS_NGINX_RUNNING}" ]; do
+    _IS_NGINX_RUNNING=$(ps aux | grep '[n]ginx' | awk '{print $2}' 2>&1)
+    echo "Waiting for Nginx graceful shutdown.."
+    sleep 3
+  done
+  echo "Nginx stopped"
+
+  echo "Stopping all PHP-FPM instances now.."
+  if [ -e "/etc/init.d/php70-fpm" ]; then
+    service php70-fpm stop &> /dev/null
   fi
-  service mysql stop
-  sleep 15
-  if [ -e "/var/run/mysqld/mysqld.pid" ] \
-    || [ -e "/var/run/mysqld/mysqld.sock" ]; then
-    service mysql stop
-    sleep 15
+  if [ -e "/etc/init.d/php56-fpm" ]; then
+    service php56-fpm stop &> /dev/null
   fi
+  if [ -e "/etc/init.d/php55-fpm" ]; then
+    service php55-fpm stop &> /dev/null
+  fi
+  if [ -e "/etc/init.d/php54-fpm" ]; then
+    service php54-fpm stop &> /dev/null
+  fi
+  if [ -e "/etc/init.d/php53-fpm" ]; then
+    service php53-fpm stop &> /dev/null
+  fi
+  # kill -9 $(ps aux | grep '[p]hp-fpm' | awk '{print $2}')
+  until [ -z "${_IS_FPM_RUNNING}" ]; do
+    _IS_FPM_RUNNING=$(ps aux | grep '[p]hp-fpm' | awk '{print $2}' 2>&1)
+    echo "Waiting for PHP-FPM graceful shutdown.."
+    sleep 3
+  done
+  echo "PHP-FPM stopped"
+
+  _IS_MYSQLD_RUNNING=$(ps aux | grep '[m]ysqld' | awk '{print $2}' 2>&1)
+  if [ ! -z "${_IS_MYSQLD_RUNNING}" ]; then
+    if [ "${_DB_SERIES}" = "10.1" ]; then
+      echo "Preparing MySQLD for quick shutdown.."
+      mysql -u root -e "SET GLOBAL innodb_max_dirty_pages_pct = 0;" &> /dev/null
+      mysql -u root -e "SET GLOBAL innodb_buffer_pool_dump_at_shutdown = 1;" &> /dev/null
+      mysql -u root -e "SET GLOBAL innodb_io_capacity = 8000;" &> /dev/null
+      mysql -u root -e "SET GLOBAL innodb_buffer_pool_dump_pct = 100;" &> /dev/null
+    fi
+    echo "Stopping MySQLD now.."
+    service mysql stop &> /dev/null
+  else
+    echo "MySQLD already stopped, it seems?"
+    echo "Nothing to do, let's quit now. Bye!"
+    rm -f /var/run/boa_wait.pid
+    rm -f /var/run/fmp_wait.pid
+    rm -f /var/run/mysql_restart_running.pid
+    exit 1
+  fi
+
+  until [ -z "${_IS_MYSQLD_RUNNING}" ]; do
+    _IS_MYSQLD_RUNNING=$(ps aux | grep '[m]ysqld' | awk '{print $2}' 2>&1)
+    echo "Waiting for MySQLD graceful shutdown.."
+    sleep 3
+  done
+  echo "MySQLD stopped"
+  sleep 3
+
+  echo "Starting MySQLD again.."
   renice ${_B_NICE} -p $$ &> /dev/null
-  if [ ! -e "/var/run/mysqld/mysqld.sock" ]; then
-    service mysql start
-  fi
-  sleep 15
+  service mysql start &> /dev/null
+  until [ ! -z "${_IS_MYSQLD_RUNNING}" ] \
+    && [ -e "/var/run/mysqld/mysqld.sock" ]; do
+    _IS_MYSQLD_RUNNING=$(ps aux | grep '[m]ysqld' | awk '{print $2}' 2>&1)
+    echo "Waiting for MySQLD graceful start.."
+    sleep 3
+  done
+  echo "MySQLD started"
+  sleep 3
+
+  echo "Removing locks"
   rm -f /var/run/boa_wait.pid
-  rm -f /var/xdrago/log/mysql_restart_running.pid
-  touch /var/xdrago/log/last-mysql-restart-done
+  rm -f /var/run/fmp_wait.pid
+  rm -f /var/run/mysql_restart_running.pid
+  echo "Procedure completed. Bye!"
   exit 0
 fi
 ###EOF2017###
