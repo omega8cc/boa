4a76ce78443079d02329e3efa3c02a6f1658e365
Barracuda Team <admin@omega8.cc>
2017-02-28 01:22:10 +0100

    Use graceful MySQLD restart procedure for innodb_log_file_size auto-config

--- aegir/tools/system/move_sql.sh
+++ aegir/tools/system/move_sql.sh
@@ -11,15 +11,59 @@ if [ -z "${_B_NICE}" ]; then
   _B_NICE=10
 fi
 
-if [ -e "/var/run/mysql_restart_running.pid" ]; then
-  echo "MySQLD restart procedure already in progress, it seems?"
-  echo "Nothing to do, let's quit now. Bye!"
-  exit 1
-else
+create_locks() {
   echo "Creating locks.."
   touch /var/run/boa_wait.pid
-  touch /var/run/mysql_restart_running.pid
   touch /var/run/fmp_wait.pid
+  touch /var/run/mysql_restart_running.pid
+}
+
+remove_locks() {
+  echo "Removing locks.."
+  rm -f /var/run/boa_wait.pid
+  rm -f /var/run/fmp_wait.pid
+  rm -f /var/run/mysql_restart_running.pid
+}
+
+check_running() {
+  if [ -e "/var/run/mysql_restart_running.pid" ]; then
+    echo "MySQLD restart procedure in progress?"
+    echo "Nothing to do, let's quit now. Bye!"
+    exit 1
+  fi
+}
+
+start_sql() {
+  check_running
+  create_locks
+
+  _IS_MYSQLD_RUNNING=$(ps aux | grep '[m]ysqld' | awk '{print $2}' 2>&1)
+  if [ ! -z "${_IS_MYSQLD_RUNNING}" ]; then
+    echo "MySQLD already running?"
+    echo "Nothing to do. Bye!"
+    remove_locks
+    [ "$1" != "chain" ] && exit 1
+  fi
+
+  echo "Starting MySQLD again.."
+  renice ${_B_NICE} -p $$ &> /dev/null
+  service mysql start &> /dev/null
+  until [ ! -z "${_IS_MYSQLD_RUNNING}" ] \
+    && [ -e "/var/run/mysqld/mysqld.sock" ]; do
+    _IS_MYSQLD_RUNNING=$(ps aux | grep '[m]ysqld' | awk '{print $2}' 2>&1)
+    echo "Waiting for MySQLD graceful start.."
+    sleep 3
+  done
+  echo "MySQLD started"
+
+  remove_locks
+  echo "MySQLD start procedure completed"
+  [ "$1" != "chain" ] && exit 0
+}
+
+stop_sql() {
+  check_running
+  create_locks
 
   echo "Stopping Nginx now.."
   service nginx stop &> /dev/null
@@ -66,12 +110,10 @@ else
     echo "Stopping MySQLD now.."
     service mysql stop &> /dev/null
   else
-    echo "MySQLD already stopped, it seems?"
-    echo "Nothing to do, let's quit now. Bye!"
-    rm -f /var/run/boa_wait.pid
-    rm -f /var/run/fmp_wait.pid
-    rm -f /var/run/mysql_restart_running.pid
-    exit 1
+    echo "MySQLD already stopped?"
+    echo "Nothing to do. Bye!"
+    remove_locks
+    [ "$1" != "chain" ] && exit 1
   fi
 
   until [ -z "${_IS_MYSQLD_RUNNING}" ]; do
@@ -80,25 +122,25 @@ else
     sleep 3
   done
   echo "MySQLD stopped"
-  sleep 3
 
-  echo "Starting MySQLD again.."
-  renice ${_B_NICE} -p $$ &> /dev/null
-  service mysql start &> /dev/null
-  until [ ! -z "${_IS_MYSQLD_RUNNING}" ] \
-    && [ -e "/var/run/mysqld/mysqld.sock" ]; do
-    _IS_MYSQLD_RUNNING=$(ps aux | grep '[m]ysqld' | awk '{print $2}' 2>&1)
-    echo "Waiting for MySQLD graceful start.."
-    sleep 3
-  done
-  echo "MySQLD started"
-  sleep 3
+  remove_locks
+  echo "MySQLD stop procedure completed"
+  [ "$1" != "chain" ] && exit 0
+}
 
-  echo "Removing locks"
-  rm -f /var/run/boa_wait.pid
-  rm -f /var/run/fmp_wait.pid
-  rm -f /var/run/mysql_restart_running.pid
-  echo "Procedure completed. Bye!"
+restart_sql() {
+  stop_sql "chain"
+  start_sql "chain"
+  remove_locks
   exit 0
-fi
+}
+
+case "$1" in
+  restart) restart_sql ;;
+  start)   start_sql "only" ;;
+  stop)    stop_sql "only" ;;
+  *)       restart_sql
+  ;;
+esac
+
 ###EOF2017###
