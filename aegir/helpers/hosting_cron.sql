d9d397c17202817dfcbc9dce421432243f8794f8
Barracuda Team <admin@omega8.cc>
2018-03-29 18:32:05 +0200

    Use utf8mb4/utf8mb4_general_ci by default

--- aegir/helpers/hosting_cron.sql
+++ aegir/helpers/hosting_cron.sql
@@ -2,4 +2,4 @@ CREATE TABLE IF NOT EXISTS `hosting_cron` (
   `nid` int(10) unsigned NOT NULL DEFAULT '0',
   `cron_interval` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`nid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
