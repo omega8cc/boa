1f963cfd5095253b088e76db4b6f84847f72d632
BOA Dev Team <boa@omega8.cc>
2019-05-07 22:17:39 +0200

    Use our mirrors for RVM keys — fixes #1363
    Hopefully..

--- aegir/tools/system/manage_ltd_users.sh
+++ aegir/tools/system/manage_ltd_users.sh
@@ -334,16 +334,18 @@ enable_chattr() {
       fi
       if [ ! -x "/home/${UQ}/.rvm/bin/rvm" ]; then
         touch /var/run/manage_rvm_users.pid
-        su -s /bin/bash - ${UQ} -c "${_GPG} --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
-        su -s /bin/bash - ${UQ} -c "${_GPG} --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
-        su -s /bin/bash - ${UQ} -c "\curl -sSL https://rvm.io/mpapis.asc | ${_GPG} --import -"
-        su -s /bin/bash - ${UQ} -c "\curl -sSL https://rvm.io/pkuczynski.asc | ${_GPG} --import -"
-        su -s /bin/bash   ${UQ} -c "\curl -sSL ${urlHmr}/helpers/rvm-installer.sh | bash -s stable"
+        su -s /bin/bash - ${UQ} -c "\curl -sSL ${urlDev}/mpapis.asc | ${_GPG} --import -"
+        su -s /bin/bash - ${UQ} -c "\curl -sSL ${urlDev}/pkuczynski.asc | ${_GPG} --import -"
+        su -s /bin/bash - ${UQ} -c "\curl -sSL ${urlHmr}/helpers/rvm-installer.sh | bash -s stable"
         su -s /bin/bash - ${UQ} -c "rvm get stable --auto-dotfiles"
         su -s /bin/bash - ${UQ} -c "echo rvm_autoupdate_flag=0 > ~/.rvmrc"
+        wait
+        su -s /bin/bash - ${UQ} -c "echo rvm_silence_path_mismatch_check_flag=1 >> ~/.rvmrc"
         rm -f /var/run/manage_rvm_users.pid
       fi
       su -s /bin/bash - ${UQ} -c "echo rvm_autoupdate_flag=0 > ~/.rvmrc"
+      wait
+      su -s /bin/bash - ${UQ} -c "echo rvm_silence_path_mismatch_check_flag=1 >> ~/.rvmrc"
       if [ ! -e "/home/${UQ}/.rvm/rubies/default" ]; then
         if [ -x "/bin/websh" ] && [ -L "/bin/sh" ]; then
           _WEB_SH=$(readlink -n /bin/sh 2>&1)
