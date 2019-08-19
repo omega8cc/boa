3994275cf80fee83fc1bf3715bfc8a4797be3fdf
BOA Dev Team <boa@omega8.cc>
2019-08-20 00:10:42 +0200

    Update sucuri _IPS

--- aegir/tools/system/guest-water.sh
+++ aegir/tools/system/guest-water.sh
@@ -169,7 +169,7 @@ whitelist_ip_sucuri() {
     wait
   fi
 
-  _IPS="192.88.134.0/23 185.93.228.0/22 66.248.200.0/22"
+  _IPS="192.88.134.0/23 185.93.228.0/22 66.248.200.0/22 208.109.0.0/22"
 
   for _IP in ${_IPS}; do
     echo checking sucuri ${_IP} now...
