8ed950df2c4e2866a4e8ba7d60b3f29d77279b9f
BOA Dev Team <boa@omega8.cc>
2020-07-03 13:50:53 +0200

    Serial 401prodQ98

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -38,7 +38,7 @@ if [ -e "/root/.pause_tasks_maint.cnf" ]; then
   exit 0
 fi
 
-_X_SE="401prodQ97"
+_X_SE="401prodQ98"
 _WEBG=www-data
 _OSR=$(lsb_release -sc 2>&1)
 _SSL_ITD=$(openssl version 2>&1 \
