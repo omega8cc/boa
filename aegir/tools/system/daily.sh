4ac83358ba3126df476fb196e9f4ddb81878c559
BOA Dev Team <boa@omega8.cc>
2020-09-16 03:38:26 +0200

    Fast SQL export/import requires single task mode

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -3211,7 +3211,7 @@ action() {
           fi
           run_drush8_hmr_cmd "${vSet} hosting_ignore_default_profiles 0"
           run_drush8_hmr_cmd "${vSet} hosting_queue_tasks_frequency 1"
-          run_drush8_hmr_cmd "${vSet} hosting_queue_tasks_items 2"
+          run_drush8_hmr_cmd "${vSet} hosting_queue_tasks_items 1"
           run_drush8_hmr_cmd "${vSet} hosting_delete_force 0"
           run_drush8_hmr_cmd "${vSet} aegir_backup_export_path ${User}/backup-exports"
           run_drush8_hmr_cmd "fr hosting_custom_settings -y"
