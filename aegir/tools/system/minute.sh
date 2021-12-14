aceb70ee50fe9f751c0203ca1534cd0973d26ae8
BOA Dev Team <boa@omega8.cc>
2021-12-14 03:53:31 +0100

    Less aggressive gpg-agent ghosts monitoring

--- aegir/tools/system/minute.sh
+++ aegir/tools/system/minute.sh
@@ -453,13 +453,13 @@ if [ `ps aux | grep -v "grep" | grep --count "php-fpm: master process"` -gt "6" 
     /var/xdrago/log/php-fpm-master-count.kill.log
 fi
 
-if [ `ps aux | grep -v "grep" | grep --count "dirmngr"` -gt "3" ]; then
+if [ `ps aux | grep -v "grep" | grep --count "dirmngr"` -gt "5" ]; then
   kill -9 $(ps aux | grep '[d]irmngr' | awk '{print $2}') &> /dev/null
   echo "$(date 2>&1) Too many dirmngr processes killed" >> \
     /var/xdrago/log/dirmngr-count.kill.log
 fi
 
-if [ `ps aux | grep -v "grep" | grep --count "gpg-agent"` -gt "2" ]; then
+if [ `ps aux | grep -v "grep" | grep --count "gpg-agent"` -gt "5" ]; then
   kill -9 $(ps aux | grep '[g]pg-agent' | awk '{print $2}') &> /dev/null
   echo "$(date 2>&1) Too many gpg-agent processes killed" >> \
     /var/xdrago/log/gpg-agent-count.kill.log
