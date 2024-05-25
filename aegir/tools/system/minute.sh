6e95e2d358010d042a3ce1696b4ddfd1667c151d
BOA Dev Team <boa@omega8.cc>
2024-05-25 05:15:08 +0200

    Sync max limit for php-fpm

--- aegir/tools/system/minute.sh
+++ aegir/tools/system/minute.sh
@@ -410,7 +410,7 @@ if [ `ps aux | grep -v "grep" | grep --count "/usr/sbin/cron"` -gt "1" ]; then
     /var/xdrago/log/cron-count.kill.log
 fi
 
-if [ `ps aux | grep -v "grep" | grep --count "php-fpm: master process"` -gt "9" ]; then
+if [ `ps aux | grep -v "grep" | grep --count "php-fpm: master process"` -gt "10" ]; then
   kill -9 $(ps aux | grep '[p]hp-fpm' | awk '{print $2}') &> /dev/null
   echo "$(date 2>&1) Too many PHP-FPM master processes killed" >> \
     /var/xdrago/log/php-fpm-master-count.kill.log
