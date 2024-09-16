7c2b6101a01a7d96ada85007ca2b0c0cb0aa5088
BOA Dev Team <boa@omega8.cc>
2024-09-16 23:52:50 +0200

    Sync load_control()

--- aegir/tools/system/purge_binlogs.sh
+++ aegir/tools/system/purge_binlogs.sh
@@ -66,16 +66,12 @@ count_cpu() {
 }
 
 load_control() {
-  if [ -e "/root/.barracuda.cnf" ]; then
-    source /root/.barracuda.cnf
-    _CPU_SPIDER_RATIO=${_CPU_SPIDER_RATIO//[^0-9]/}
-  fi
-  if [ -z "${_CPU_SPIDER_RATIO}" ]; then
-    _CPU_SPIDER_RATIO=6
-  fi
+  [ -e "/root/.barracuda.cnf" ] && source /root/.barracuda.cnf
+  export _CPU_MAX_RATIO=${_CPU_MAX_RATIO//[^0-9]/}
+  : "${_CPU_MAX_RATIO:=6}"
   _O_LOAD=$(awk '{print $1*100}' /proc/loadavg 2>&1)
   _O_LOAD=$(( _O_LOAD / _CPU_NR ))
-  _O_LOAD_MAX=$(( 99 * _CPU_SPIDER_RATIO ))
+  _O_LOAD_MAX=$(( 100 * _CPU_MAX_RATIO ))
 }
 
 action() {
