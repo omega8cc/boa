f9c10e12bb1c89dad5bda63bc7459a2701e0ec22
BOA Dev Team <boa@omega8.cc>
2024-09-17 23:18:53 +0200

    Add incident_email_report() to second.sh

--- aegir/tools/system/second.sh
+++ aegir/tools/system/second.sh
@@ -10,14 +10,50 @@ export HOME=${HOME}
 
 pthVhstd="/var/aegir/config/server_master/nginx/vhost.d"
 
+pthOml="/var/xdrago/log/high.load.incident.log"
+
 if [ -e "/root/.proxy.cnf" ]; then
   exit 0
 fi
+
+check_root() {
+  if [ `whoami` = "root" ]; then
+    [ -e "/root/.barracuda.cnf" ] && source /root/.barracuda.cnf
+    chmod a+w /dev/null
+  else
+    echo "ERROR: This script should be run as a root user"
+    exit 1
+  fi
+}
+check_root
+
+export _B_NICE=${_B_NICE//[^0-9]/}
+: "${_B_NICE:=10}"
+
+export _CPU_SPIDER_RATIO=${_CPU_SPIDER_RATIO//[^0-9]/}
+: "${_CPU_SPIDER_RATIO:=3}"
+
+export _CPU_MAX_RATIO=${_CPU_MAX_RATIO//[^0-9]/}
+: "${_CPU_MAX_RATIO:=6}"
+
+export _CPU_CRIT_RATIO=${_CPU_CRIT_RATIO//[^0-9]/}
+: "${_CPU_CRIT_RATIO:=9}"
+
+export _INCIDENT_EMAIL_REPORT=${_INCIDENT_EMAIL_REPORT//[^A-Z]/}
+: "${_INCIDENT_EMAIL_REPORT:=YES}"
 
 if [ $(pgrep -f second.sh | grep -v "^$$" | wc -l) -gt 4 ]; then
   echo "Too many second.sh running $(date 2>&1)" >> /var/xdrago/log/too.many.log
   exit 0
 fi
+
+incident_email_report() {
+  if [ -n "${_MY_EMAIL}" ] && [ "${_INCIDENT_EMAIL_REPORT}" = "YES" ]; then
+    hName=$(cat /etc/hostname 2>&1)
+    echo "Sending Incident Report Email on $(date 2>&1)" >> ${pthOml}
+    s-nail -s "Incident Report: ${1} on ${hName} at $(date 2>&1)" ${_MY_EMAIL} < ${pthOml}
+  fi
+}
 
 hold() {
   killall -9 nginx
@@ -31,13 +67,20 @@ hold() {
 terminate() {
   if [ ! -e "/run/boa_run.pid" ]; then
     killall -9 php drush.php wget curl &> /dev/null
-    echo "$(date 2>&1)" >> /var/xdrago/log/second.terminate.log
+    thisErrLog="$(date 2>&1) System Load $1 PHP/Wget/cURL terminated"
+    echo ${thisErrLog} >> ${pthOml}
+    incident_email_report "System Load $1 PHP/Wget/cURL terminated"
+    echo >> ${pthOml}
   fi
 }
 
 nginx_high_load_on() {
   mv -f /data/conf/nginx_high_load_off.conf /data/conf/nginx_high_load.conf
   service nginx reload &> /dev/null
+  thisErrLog="$(date 2>&1) nginx_high_load_on $1"
+  echo ${thisErrLog} >> ${pthOml}
+  incident_email_report "nginx_high_load_on $1"
+  echo >> ${pthOml}
 }
 
 nginx_high_load_off() {
@@ -299,13 +342,17 @@ manage_ip_auth_access() {
     fi
   fi
   rm -f /var/backups/.auth.IP.list.tmp
+  thisErrLog="$(date 2>&1) nginx_high_load_off $1"
+  echo ${thisErrLog} >> ${pthOml}
+  incident_email_report "nginx_high_load_off $1"
+  echo >> ${pthOml}
 }
 
 proc_control() {
   if [ "${_O_LOAD}" -ge "${_O_LOAD_MAX}" ]; then
-    hold
+    hold "${_O_LOAD}/${_O_LOAD_MAX}"
   elif [ "${_F_LOAD}" -ge "${_F_LOAD_MAX}" ]; then
-    hold
+    hold "${_F_LOAD}/${_F_LOAD_MAX}"
   else
     echo "load is ${_O_LOAD}:${_F_LOAD} while \
       maxload is ${_O_LOAD_MAX}:${_F_LOAD_MAX}"
@@ -352,21 +399,21 @@ load_control() {
   if [ "${_O_LOAD}" -ge "${_O_LOAD_SPR}" ] \
     && [ "${_O_LOAD}" -lt "${_O_LOAD_MAX}" ] \
     && [ -e "/data/conf/nginx_high_load_off.conf" ]; then
-    nginx_high_load_on
+    nginx_high_load_on "${_O_LOAD}/${_O_LOAD_MAX}"
   elif [ "${_F_LOAD}" -ge "${_F_LOAD_SPR}" ] \
     && [ "${_F_LOAD}" -lt "${_F_LOAD_MAX}" ] \
     && [ -e "/data/conf/nginx_high_load_off.conf" ]; then
-    nginx_high_load_on
+    nginx_high_load_on "${_F_LOAD}/${_F_LOAD_MAX}"
   elif [ "${_O_LOAD}" -lt "${_O_LOAD_SPR}" ] \
     && [ "${_F_LOAD}" -lt "${_F_LOAD_SPR}" ] \
     && [ -e "/data/conf/nginx_high_load.conf" ]; then
-    nginx_high_load_off
+    nginx_high_load_off "${_O_LOAD}/${_F_LOAD_SPR}"
   fi
 
   if [ "${_O_LOAD}" -ge "${_O_LOAD_CRT}" ]; then
-    terminate
+    terminate "${_O_LOAD}/${_O_LOAD_CRT}"
   elif [ "${_F_LOAD}" -ge "${_F_LOAD_CRT}" ]; then
-    terminate
+    terminate "${_F_LOAD}/${_F_LOAD_CRT}"
   fi
 
   proc_control
@@ -392,25 +439,6 @@ count_cpu() {
     _CPU_NR=1
   fi
 }
-
-[ -e "/root/.barracuda.cnf" ] && source /root/.barracuda.cnf
-
-export _B_NICE=${_B_NICE//[^0-9]/}
-: "${_B_NICE:=10}"
-
-export _CPU_SPIDER_RATIO=${_CPU_SPIDER_RATIO//[^0-9]/}
-: "${_CPU_SPIDER_RATIO:=3}"
-
-export _CPU_MAX_RATIO=${_CPU_MAX_RATIO//[^0-9]/}
-: "${_CPU_MAX_RATIO:=6}"
-
-export _CPU_CRIT_RATIO=${_CPU_CRIT_RATIO//[^0-9]/}
-: "${_CPU_CRIT_RATIO:=9}"
-
-if [ ! -e "/var/tmp/fpm" ]; then
-  mkdir -p /var/tmp/fpm
-  chmod 777 /var/tmp/fpm
-fi
 
 count_cpu
 load_control
