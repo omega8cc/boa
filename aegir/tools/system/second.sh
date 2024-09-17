64e4fa567a8d7d61dad2e409baffec52136ecfe3
BOA Dev Team <boa@omega8.cc>
2024-09-17 06:52:57 +0200

    Allow two second.sh

--- aegir/tools/system/second.sh
+++ aegir/tools/system/second.sh
@@ -14,7 +14,7 @@ if [ -e "/root/.proxy.cnf" ]; then
   exit 0
 fi
 
-if [ $(pgrep -f second.sh | grep -v "^$$" | wc -l) -gt 1 ]; then
+if [ $(pgrep -f second.sh | grep -v "^$$" | wc -l) -gt 2 ]; then
   echo "Too many second.sh running" >> /var/xdrago/log/too.many.log
   exit 0
 fi
