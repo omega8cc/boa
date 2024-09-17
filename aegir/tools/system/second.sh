ee53075e6f1444f1e4d1d5afecc0b71d8e2677f1
BOA Dev Team <boa@omega8.cc>
2024-09-17 23:16:24 +0200

    Sync Too many second.sh

--- aegir/tools/system/second.sh
+++ aegir/tools/system/second.sh
@@ -14,7 +14,7 @@ if [ -e "/root/.proxy.cnf" ]; then
   exit 0
 fi
 
-if [ $(pgrep -f second.sh | grep -v "^$$" | wc -l) -gt 3 ]; then
+if [ $(pgrep -f second.sh | grep -v "^$$" | wc -l) -gt 4 ]; then
   echo "Too many second.sh running $(date 2>&1)" >> /var/xdrago/log/too.many.log
   exit 0
 fi
