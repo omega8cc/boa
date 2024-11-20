85d413505c62fd599f2bcf14f407692c98ad052a
BOA Dev Team <boa@omega8.cc>
2024-11-20 19:38:55 +0100

    Use strict path definition for directories

--- aegir/tools/system/manage_ltd_users.sh
+++ aegir/tools/system/manage_ltd_users.sh
@@ -771,8 +771,8 @@ _manage_sec_access_paths() {
 #for _Domain in `find ${_Client}/ -maxdepth 1 -mindepth 1 -type l -printf %P\\n | sort`
 for _Domain in `find ${_Client}/ -maxdepth 1 -mindepth 1 -type l | sort`; do
   _rawDom=$(echo ${_Domain} | cut -d'/' -f7 | awk '{ print $1}' 2>&1)
-  _STATIC_FILES="${_pthParen_tUsr}/static/files/${_rawDom}.files"
-  _STATIC_PRIVATE="${_pthParen_tUsr}/static/files/${_rawDom}.private"
+  _STATIC_FILES="${_pthParen_tUsr}/static/files/${_rawDom}.files/"
+  _STATIC_PRIVATE="${_pthParen_tUsr}/static/files/${_rawDom}.private/"
   _NEW_STATIC_FILES="${_pthParen_tUsr}/static/files/${_rawDom}/"
   _PATH_DOM=$(readlink -n ${_Domain} 2>&1)
   _PATH_DOM=$(echo -n ${_PATH_DOM} | tr -d "\n" 2>&1)
