7ff98909922b290e4626bb67e075626c58058dd1
BOA Dev Team <boa@omega8.cc>
2024-11-25 09:37:35 +0100

    Sync spacing in templates

--- aegir/tools/system/backup/run/create_readme.sh
+++ aegir/tools/system/backup/run/create_readme.sh
@@ -43,8 +43,8 @@ Amazon S3 (Standard, One Zone, Standard-IA)
   export AWS_ACCESS_KEY_ID="your_aws_access_key"
   export AWS_SECRET_ACCESS_KEY="your_aws_secret_key"
   export AWS_REGION="your_aws_region"  # E.g., "us-east-1"
-  export KEEP_WITHIN="3M"  # Retain backups from the last 3 months
-  export FULL_BACKUP_FREQUENCY="7D"  # Create a full backup every 7 days
+  export KEEP_WITHIN="3M"              # Retain backups from the last 3 months
+  export FULL_BACKUP_FREQUENCY="7D"    # Create a full backup every 7 days
 
 Google Cloud Storage
   File: gcs.txt
