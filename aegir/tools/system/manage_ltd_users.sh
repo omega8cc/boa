22bdeec9eb502121ab2ab476458bbeb145d955ed
BOA Dev Team <boa@omega8.cc>
2024-12-02 21:54:41 +0100

    Rename to _forward_to_dash

--- aegir/tools/system/manage_ltd_users.sh
+++ aegir/tools/system/manage_ltd_users.sh
@@ -2422,7 +2422,7 @@ else
     _WEB_SH=$(readlink -n /bin/sh 2>&1)
     _WEB_SH=$(echo -n ${_WEB_SH} | tr -d "\n" 2>&1)
     if [ -x "/opt/local/bin/websh" ] \
-      && grep -i '_forward_to_shell' /opt/local/bin/websh &> /dev/null; then
+      && grep -i '_forward_to_dash' /opt/local/bin/websh &> /dev/null; then
       if [ "${_WEB_SH}" != "/opt/local/bin/websh" ]; then
         ln -sfn /opt/local/bin/websh /bin/sh
         if [ -e "/usr/bin/sh" ]; then
