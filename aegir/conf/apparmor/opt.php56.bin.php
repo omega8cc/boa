42972dd10bef8dee4a6a06876a11cfef695e6e2b
BOA Dev Team <boa@omega8.cc>
2025-02-01 14:44:49 +0100

    Remove vendor/bin/drush from AppArmor profiles

--- aegir/conf/apparmor/opt.php56.bin.php
+++ aegir/conf/apparmor/opt.php56.bin.php
@@ -115,7 +115,6 @@
   /bin/tar mrix,
   /bin/touch mrix,
   /opt/local/bin/websh mrix,
-  /data/disk/*/**/vendor/bin/drush mrix,
   /data/disk/*/**/vendor/drush/drush/drush.php mrix,
   /etc/init.d/nginx mrix,
   /sbin/killall5 mrix,
