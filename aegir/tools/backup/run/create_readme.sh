651094fb72dbe32e000a57549e370a01c3f76046
BOA Dev Team <boa@omega8.cc>
2025-02-06 01:39:49 +0100

    Sync permissions

--- aegir/tools/backup/run/create_readme.sh
+++ aegir/tools/backup/run/create_readme.sh
@@ -3,7 +3,7 @@
 export HOME=/root
 export SHELL=/bin/bash
 export PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/usr/bin:/usr/sbin:/bin:/sbin
-export _sPid="f73"
+export _sPid="f72"
 
 # Directory for storing README files
 _BASE_DIR="/data/disk"
