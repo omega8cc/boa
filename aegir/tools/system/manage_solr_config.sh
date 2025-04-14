56b2e0c236a10b1e8d023db974737bdc6e35c3d4
BOA Dev Team <boa@omega8.cc>
2025-04-14 21:16:40 +0200

    Do not modify solr.replication.masterUrl in Solr 7

--- aegir/tools/system/manage_solr_config.sh
+++ aegir/tools/system/manage_solr_config.sh
@@ -695,12 +695,9 @@ _fix_solr7_core() {
   if [ -e "${file}" ]; then
     local _test_id
     _test_id=$(grep "solr7" "${file}" 2>&1)
-    _test_port=$(grep "9077" "${file}" 2>&1)
-    if [[ ! "${_test_id}" =~ "solr7" ]] || [[ ! "${_test_port}" =~ "9077" ]]; then
-      sed -i "s/^solr\.replication\.masterUrl.*//g" "${file}"
+    if [[ ! "${_test_id}" =~ "solr7" ]]; then
       sed -i "s/^solr\.install\.dir.*//g" "${file}"
       sed -i "s/^solr\.contrib\.dir.*//g" "${file}"
-      echo "solr.replication.masterUrl=http://localhost:9077/solr" >> "${file}"
       echo "solr.install.dir=/opt/solr7" >> "${file}"
       sed -i "/^$/d" "${file}"
       echo "Fixed ${file}"
@@ -715,12 +712,9 @@ _fix_solr7_cnf() {
     for _pRp in `find /var/solr7/data/oct.*/conf/solrcore.properties -maxdepth 1 | sort`; do
       if [ -e "${_pRp}" ]; then
         _PRP_TEST_ID=$(grep "solr7" ${_pRp} 2>&1)
-        _PRP_TEST_PORT=$(grep "9077" ${_pRp} 2>&1)
-        if [[ ! "${_PRP_TEST_ID}" =~ "solr7" ]] || [[ ! "${_PRP_TEST_PORT}" =~ "9077" ]]; then
-          sed -i "s/^solr\.replication\.masterUrl.*//g" ${_pRp}
+        if [[ ! "${_PRP_TEST_ID}" =~ "solr7" ]]; then
           sed -i "s/^solr\.install\.dir.*//g" ${_pRp}
           sed -i "s/^solr\.contrib\.dir.*//g" ${_pRp}
-          echo "solr.replication.masterUrl=http://localhost:9077/solr" >> ${_pRp}
           echo "solr.install.dir=/opt/solr7" >> ${_pRp}
           sed -i "/^$/d" ${_pRp}
           echo "Fixed ${_pRp}"
