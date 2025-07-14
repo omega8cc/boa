5dec3686719d3ff84d507ef32e13877304235f1b
BOA Dev Team <boa@omega8.cc>
2025-07-15 01:11:40 +0200

    Sync path to PHAR at /usr/local/bin/drush8

--- aegir/tools/system/manage_ltd_users.sh
+++ aegir/tools/system/manage_ltd_users.sh
@@ -1975,7 +1975,7 @@ _manage_user() {
         | awk '{ print $3}' \
         | sed "s/[\,']//g" 2>&1)
       if [ -e "${_THIS_HM_PLR}/modules/path_alias_cache" ] \
-        && [ -x "/opt/tools/drush/8/drush/drush.php" ]; then
+        && [ -x "/usr/local/bin/drush8" ]; then
         if [ -x "/opt/php56/bin/php" ]; then
           echo 5.6 > ${_dscUsr}/static/control/cli.info
         fi
