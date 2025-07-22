ad3edcb3751df75933e600528b7eb4c63c9b439a
BOA Dev Team <boa@omega8.cc>
2025-07-23 00:38:45 +0200

    Add cv.phar mrix

--- aegir/conf/apparmor/opt.php82.bin.php
+++ aegir/conf/apparmor/opt.php82.bin.php
@@ -209,6 +209,7 @@
   /data/disk/*/tools/drush/** r,
   /opt/tools/drush/** mrix,
   /usr/local/bin/drush8.phar mrix,
+  /usr/local/bin/cv.phar mrix,
   /var/aegir/drush/ r,
   /var/aegir/drush/* mrix,
   /var/aegir/drush/** r,
