97f47787e50f1bf190d6f152b9de894c5c8966d5
BOA Dev Team <boa@omega8.cc>
2025-07-25 23:46:01 +0200

    Tag 570ltsT11

--- aegir/tools/system/manage_solr_config.sh
+++ aegir/tools/system/manage_solr_config.sh
@@ -4,7 +4,7 @@ export HOME=/root
 export SHELL=/bin/bash
 export PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/usr/bin:/usr/sbin:/bin:/sbin
 export _tRee=lts
-export _xSrl=570devT11
+export _xSrl=570ltsT11
 
 if [ -e "/root/.proxy.cnf" ]; then
   exit 0
