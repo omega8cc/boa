f9decc31308778c9b28579bde8f59b37d1eb8448
BOA Dev Team <boa@omega8.cc>
2025-07-28 01:02:08 +0200

    Removing all .drush.inc files in the codebases
    This cleanup is required both for Drush 8 compatibility reasons and security reasons.

--- aegir/tools/bin/fix-drupal-platform-permissions.sh
+++ aegir/tools/bin/fix-drupal-platform-permissions.sh
@@ -71,8 +71,9 @@ find ${drupal_root}/{modules,themes,libraries,includes,misc,profiles,core} -type
 printf "Setting permissions of all codebase files inside "${drupal_root}"...\n"
 find ${drupal_root}/{modules,themes,libraries,includes,misc,profiles,core} -type f -exec chmod 0664 {} \;
 
-printf "Locking permissions of all .drush.inc files inside codebase "${drupal_root}"...\n"
-find ${drupal_root}/modules -type f -name "*.drush.inc" -exec chmod 0000 {} \;
+printf "Removing all .drush.inc files inside codebase "${drupal_root}"...\n"
+find ${drupal_root}/modules/contrib -type f -name "*.drush.inc" -exec rm -f {} \;
+find ${drupal_root}/sites/*/modules -type f -name "*.drush.inc" -exec rm -f {} \;
 
 if [ -e "${drupal_root}/vendor" ]; then
   printf "Setting permissions of all codebase directories inside "${drupal_root}/vendor"...\n"
