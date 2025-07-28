023eb54852aab497f6efd81d127ee1a75ed34f14
BOA Dev Team <boa@omega8.cc>
2025-07-29 00:22:19 +0200

    570devT12

--- aegir/tools/system/daily.sh
+++ aegir/tools/system/daily.sh
@@ -4,7 +4,7 @@ export HOME=/root
 export SHELL=/bin/bash
 export PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/usr/bin:/usr/sbin:/bin:/sbin
 export _tRee=dev
-export _xSrl=570devT11
+export _xSrl=570devT12
 
 _check_root() {
   if [ "$(id -u)" -eq 0 ]; then
