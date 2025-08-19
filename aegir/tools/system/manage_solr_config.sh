6cdd83ae1fb7a199bd51ed38b0251071b2254af7
BOA Dev Team <boa@omega8.cc>
2025-08-19 12:41:58 +0200

    Remove exceptions in the two manage_ agents

--- aegir/tools/system/manage_solr_config.sh
+++ aegir/tools/system/manage_solr_config.sh
@@ -6,7 +6,6 @@ export PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/usr/bin:/usr/sbin:/bi
 export _tRee=lts
 export _xSrl=570ltsT05
 
-[ -e "/root/.look.like.jenkins.cnf" ] && exit 0
 [ -e "/root/.proxy.cnf" ] && exit 0
 
 _crlGet="-L --max-redirs 3 -k -s --retry 9 --retry-delay 9 -A iCab"
