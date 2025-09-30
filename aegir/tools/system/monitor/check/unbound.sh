70254e444906df4a102c8a2a7fd4183a90277ff4
BOA Dev Team <boa@omega8.cc>
2025-09-30 22:25:52 +0200

    Sync /etc/resolv.conf format

--- aegir/tools/system/monitor/check/unbound.sh
+++ aegir/tools/system/monitor/check/unbound.sh
@@ -117,7 +117,8 @@ _unbound_check_fix() {
       touch /run/wait-unbound.pid
       sleep 3
       rm -f /etc/resolv.conf
-      echo "nameserver 127.0.0.1" > /etc/resolv.conf
+      echo "### BOA-DNS-Config ###" > /etc/resolv.conf
+      echo "nameserver 127.0.0.1" >> /etc/resolv.conf
       echo "nameserver 1.1.1.1" >> /etc/resolv.conf
       echo "nameserver 8.8.8.8" >> /etc/resolv.conf
       echo "nameserver 9.9.9.9" >> /etc/resolv.conf
