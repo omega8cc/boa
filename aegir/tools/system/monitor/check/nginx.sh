e1dc0993c3182aaf8bc0da12049da129bd3d60cf
BOA Dev Team <boa@omega8.cc>
2025-09-30 21:16:26 +0200

    Update Multiple Nginx master processes detection

--- aegir/tools/system/monitor/check/nginx.sh
+++ aegir/tools/system/monitor/check/nginx.sh
@@ -124,9 +124,8 @@ _nginx_heatlh_check_fix() {
   if [ "${_NGINX_RESTARTED}" = false ]; then
     _MASTER_COUNT=$(pgrep -fc 'nginx: master process')
     if [ "${_MASTER_COUNT}" -gt 1 ]; then
-      echo "Multiple Nginx master processes detected. Possible stuck processes."
-      echo "$(date) NGX multiple master processes detected" >> ${_pthOml}
-      echo "$(date) NGX ${_NGINX_PROCESSES}" >> ${_pthOml}
+      echo "Multiple (${_MASTER_COUNT}) Nginx master processes detected. Possible stuck processes."
+      echo "$(date) NGX multiple (${_MASTER_COUNT}) master processes detected" >> ${_pthOml}
       _restart_nginx "_MASTER_COUNT ${_MASTER_COUNT}"
     fi
   fi
