5a7f40575e6961659c69e968544dbeea08d30991
BOA Dev Team <boa@omega8.cc>
2026-06-04 22:54:08 +0200

    Serial 593ltsT05

--- aegir/tools/system/manage_solr_config.sh
+++ aegir/tools/system/manage_solr_config.sh
@@ -3,8 +3,8 @@
 export HOME=/root
 export SHELL=/bin/bash
 export PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/libexec
-export _tRee=dev
-export _xSrl=593devT05
+export _tRee=lts
+export _xSrl=593ltsT05
 
 [ -e "/root/.proxy.cnf" ] && exit 0
 
